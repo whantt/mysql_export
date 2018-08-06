001c84d49191f8b6c7783f283681b08d1102a440
lazzyfu <1126227133@qq.com>
2018-08-06 15:49:29 +0800

    Update export_data.py

--- export_data.py
+++ export_data.py
@@ -84,7 +84,7 @@ class ExportData(object):
         # 分页数量：10000
         # rewrite_rule = re.compile('([\s\S]*)SELECT([\s\S]*) FROM ([\s\S]*)', re.I)
         # count_query = rewrite_rule.sub(r'SELECT count(*) as count FROM \3', sql)
-        count_query = f"select count(*) as count from {sql} as subquery"
+        count_query = f"select count(*) as count from ({sql}) as subquery"
 
         self.conn.cursorclass = pymysql.cursors.DictCursor
         with self.conn.cursor() as cursor:
